cc4830230b33a9a4a8f21c48cfb629641f1b5c2c
Yash Gupta <yash@cvt.digital>
2025-12-08 18:12:20 +0530

    chore: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname, // Fix project root detection
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://13.201.99.3:4000/api/:path*", // Your EC2 backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
