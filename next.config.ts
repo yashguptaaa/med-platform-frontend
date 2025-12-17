3dfb2978b85a9481fc67ddce1e1f7d6fd77e07c0
Yash Gupta <yash@cvt.digital>
2025-12-17 16:25:06 +0530

    fix: bad gateway error

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const INSTANCE_IP =
+  process.env.EIP || "15.207.54.240";
+
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname, // Fix project root detection
+    root: __dirname,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `http://${INSTANCE_IP}:4000/api/:path*`,
+      },
+    ];
   },
 };
 
